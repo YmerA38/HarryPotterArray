0c8c6aa83b30a7f7c72d1ced85d133e4da508492
YmerA38 <stin166c@stud.kea.dk>
2022-12-14 15:24:30 +0100

    Nevil

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,7 +20,11 @@ public class Main {
         person4.addPoints(1000);
         personer[1] = person4;
         Person person5 = new Person("Draco","Malfoy");
+        person5.addPoints(215);
         personer[4] = person5;
+        Person person6 = new Person("Nevil","Longbottom");
+        person6.addPoints(7);
+        personer[4] = person6;
         System.out.println(Arrays.toString(personer));
         for (Person person : personer){
             System.out.println(person);
